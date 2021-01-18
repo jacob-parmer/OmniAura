9b1a2d3f59b517ba7ba52ff967c3e09b838c66b3
= <ozanji345@gmail.com>
2021-01-17 19:20:27 -0600

    Python and supercollider UDP communication via OSC setup in main.scd and main.py

--- main.py
+++ main.py
@@ -36,8 +36,13 @@ class Omni():
 if __name__ == "__main__":
     OmniSynth = Omni() # initialize Omni class.
     #OmniSynth.open_stream()
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1", help="the gordon to listen")
-    parser.add_argument("--port", type=int, default=7771)
-
+    parser.add_argument("--ip", default="127.0.0.1", help="the ip to listen on")
+    parser.add_argument("--port", type=int, default=7771, help="the ip to listen on")
+    args = parser.parse_args()
+    d = dispatcher.Dispatcher()
+    d.map("/noteOn", print)
+    server = osc_server.ThreadingOSCUDPServer(
+      (args.ip, args.port), d)
+    print("Serving on {}".format(server.server_address))
+    server.serve_forever()
